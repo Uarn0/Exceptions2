3d29c1e11a5f7c5db05b11f9cadd0ba52b013cf7
Uarn0 <nazarvyginny@gmail.com>
2024-11-11 08:08:06 +0200

    Do tests

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string directoryPath = @"C:\vsprog\lessons2\ExceptionsOOP2\bin\Debug\net7.0\FolderWithImage";
+        string[] filePaths = Directory.GetFiles(directoryPath);
+        Regex regexExtForImage = new Regex(@"(bmp|gif|tiff?|jpe?g|png)$", RegexOptions.IgnoreCase);
+
+        foreach (string filePath in filePaths)
+        {
+            string fileName = Path.GetFileName(filePath);
+            if (regexExtForImage.IsMatch(Path.GetExtension(fileName)))
+            {
+                try
+                {
+                    using (Bitmap bitmap = new Bitmap(filePath))
+                    {
+                        var img = Image.FromFile(filePath);
+
+                        bitmap.RotateFlip(RotateFlipType.Rotate180FlipY);
+
+                        string newFileName = Path.Combine(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(fileName) + "-mirrored.gif");
+
+                        bitmap.Save(newFileName, System.Drawing.Imaging.ImageFormat.Gif);
+                        Console.WriteLine($"Файл створено i збережено успішно, як <<{Path.GetFileName(newFileName)}>>.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Файл {fileName} має проблеми: {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Файл {fileName} не є зображенням");
+            }
+        }
+    }
+}
